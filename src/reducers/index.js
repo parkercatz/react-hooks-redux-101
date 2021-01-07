3aa738b5b2d526370bceeac9cc09100aad6abf6b
ParkerCatz <tksm.webdesign09@gmail.com>
2021-01-07 17:09:22 +0900

    fix typo

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,10 @@
 const events = (state = [], action) => {
   switch (action.type) {
     case 'CREATE_EVENT':
-      const events = { title: action.title, body: action.body };
+      const event = { title: action.title, body: action.body };
       const length = state.length;
       const id = length === 0 ? 1 : state[length - 1].id + 1;
-      return [...state, { id, ...events }];
+      return [...state, { id, ...event }];
     case 'DELETE_EVENT':
       return state;
     case 'DELETE_ALL_EVENT':
